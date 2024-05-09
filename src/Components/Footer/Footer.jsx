1aa36143459f393e83c21bdbde3cf5c8c937f283
Cristina-Giardini <crisgiardini30@gmail.com>
2024-05-09 16:53:17 -0300

    Atualizaçao do footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-            <p className={styles["footer-text"]}> ©️ 2024 RaiaDrogasil - Todos os direitos reservados.</p>
+      <p> ©️ 2024 RaiaDrogasil - Todos os direitos reservados.</p>
     </div>
   )
 } 
