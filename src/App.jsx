e8e770091d40b3ba93fae2b77fa4842a95714964
Maria Cristina Leal Giardini <mclgiardini@rd.com.br>
2024-05-09 11:23:09 -0300

    feat:include footer

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Footer from './Components/Footer/Footer';
+import "./styles/Global.css"
+
+
+
+class App extends React.Component {
+  render() {
+
+
+  return (
+<div className="app-container">
+    <footer>
+        <Footer />
+    </footer>
+  </div>
+  )
+    
+  }  
+
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./styles.module.scss";
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+            <p className={styles["footer-text"]}> ©️ 2024 RaiaDrogasil - Todos os direitos reservados.</p>
+    </div>
+  )
+} 
