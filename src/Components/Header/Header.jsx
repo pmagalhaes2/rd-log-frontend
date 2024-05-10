146a4cc556e0f3f596ba6758ea73f144ecddfe18
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-05-10 09:41:53 -0300

    refact: update input component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles.module.scss";
-import { Icon } from '@iconify/react';
-import searchIcon from '@iconify-icons/mdi/magnify';
-import rdlog from '../../assets/images/rdlog.png';
+import rdlog from "../../assets/images/rdlog.png";
 import userIcon from "../../assets/images/user-icon.svg";
 import expandArrowIcon from "../../assets/images/expand-header-icon.svg";
+import { Input } from "../Input";
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <img className={styles.logo} src={rdlog} alt='RDLog'/>
-      
+      <img className={styles.logo} src={rdlog} alt="RDLog" />
+
       <div className={styles.searchContainer}>
-        <Icon icon={searchIcon} className={styles.searchIcon} />
-        <input className={styles.searchInput} type="text" placeholder="O que você precisa?" />
+        <Input placeholder={"O que você precisa?"} searchInput={true} />
       </div>
       <div className={styles.separator}></div>
 
+      <div className={styles.userSection}>
+        <img src={userIcon} alt="UserIcon" className={styles.userIcon} />
 
-
-      <div className={styles.userSection}>  
-        <img src={userIcon} alt='UserIcon'  className={styles.userIcon}/>
-     
         <div className={styles.userText}>
-          <span >Bem-vindo(a),</span>
-          <span className={styles.personContainer} >Cristina!</span>
+          <span>Bem-vindo(a),</span>
+          <span className={styles.personContainer}>Cristina!</span>
         </div>
-        <img className={styles.expandIcon} src={expandArrowIcon} alt='ExpandIcon'/>
+        <img
+          className={styles.expandIcon}
+          src={expandArrowIcon}
+          alt="ExpandIcon"
+        />
       </div>
-
     </header>
-  )
+  );
 }
--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,13 +1,18 @@
-import { Icon } from '@iconify/react/dist/iconify';
-import searchIcon from '@iconify-icons/mdi/magnify';
+import { Icon } from "@iconify/react/dist/iconify";
+import searchIcon from "@iconify-icons/mdi/magnify";
 
 import styles from "./Input.module.scss";
 
-export const Input = ({ onChange, placeholder, value }) => {
+export const Input = ({
+  searchInput = false,
+  onChange,
+  placeholder,
+  value,
+}) => {
   return (
     <div className={styles.container}>
-      <input onChange={onChange} placeholder={placeholder} value={value}/>
-      <Icon icon={searchIcon} className={styles.search_icon} />
+      <input onChange={onChange} placeholder={placeholder} value={value} />
+      {searchInput && <Icon icon={searchIcon} className={styles.search_icon} />}
     </div>
   );
 };
