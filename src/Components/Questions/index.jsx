e36efbee9f0e6fc6091c87e1a57d716a42982b29
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-05-09 22:27:51 -0300

    feat: handle opened question

--- src/Components/Questions/index.jsx
+++ src/Components/Questions/index.jsx
@@ -1,15 +1,39 @@
-import React from "react";
-import expandArrowIcon from "../../assets/images/expand-arrow-icon.svg";
+import React, { useState } from "react";
+import { Icon } from "@iconify/react/dist/iconify";
 
-import styles from './Questions.module.scss'
+import arrowUp from "@iconify-icons/mdi/keyboard-arrow-up";
+import arrowDown from "@iconify-icons/mdi/keyboard-arrow-down";
+
+import styles from "./Questions.module.scss";
 
 export const Questions = ({ questions }) => {
+  const [openedId, setOpenedId] = useState(null);
+
+  const handleOpened = (index) => {
+    setOpenedId(openedId === index ? null : index);
+  };
+
   return (
     <div className={styles.container}>
       {questions.map((question, index) => (
-        <div key={index}>
+        <div key={index} className={styles.question_container}>
+          <div className={styles.question_heading}>
             <p>{question}</p>
-            <img src={expandArrowIcon} alt="Ícone de seta de expansão" />
+            <Icon
+              icon={openedId === index ? arrowUp : arrowDown}
+              onClick={() => handleOpened(index)}
+            />
+          </div>
+          {openedId === index && (
+            <div className={styles.answer_container}>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+                repellendus quibusdam numquam animi modi officia nisi? Et, ab
+                obcaecati totam suscipit consequatur, libero tenetur ratione
+                adipisci, sit cumque autem recusandae?
+              </p>
+            </div>
+          )}
         </div>
       ))}
     </div>
---
+++ src/Components/Questions/questions.js
@@ -0,0 +1,11 @@
+const questions = [
+  "Quais são os horários de funcionamento da nossa empresa de logística?",
+  "Como faço para rastrear minha entrega?",
+  "Quais são as opções de pagamento disponíveis para os serviços de entrega?",
+  "Qual é a distância máxima para as entregas ao cliente final?",
+  "Como posso solicitar um transporte para minha empresa?",
+  "Posso agendar uma coleta de produtos em minha empresa?",
+  "Quais documentos são necessários para contratar os serviços de transporte?",
+];
+
+export default questions;
