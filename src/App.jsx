3125135c23b9d14472bb7a951133b3a53041478c
Cristina-Giardini <crisgiardini30@gmail.com>
2024-05-13 18:26:21 -0300

    criação da pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles/Global.css";
 import { Support } from "./pages/Support";
-
+import { Login } from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Support />
+     <Login/>
     </div>
   );
 }
