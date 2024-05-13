aa6f59d1d5bb0b27c39c04fa529d0a262bf880d3
thamirescandidabarbosa <thamy291@gmail.com>
2024-05-13 09:13:29 -0300

    Menu icon, spacing and h3 heading insertion

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import styles from './Menu.module.scss'; 
 import menuIcon from '../../assets/images/image 7.png';
+import pack from '../../assets/images/Pack.png';
+import dash from '../../assets/images/Dash.png';
+import truck from '../../assets/images/Truck.png';
+import maps from '../../assets/images/maps.png';
+import faqs from '../../assets/images/FaQs.png';
 
 function MenuComponent() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,14 +18,15 @@ function MenuComponent() {
     <div className={`${styles['menu-lateral']} ${menuOpen ? styles['menu-open'] : ''}`}>
       <div className={styles['menu-icon']} onClick={toggleMenu}>
         <img src={menuIcon} alt="Menu" />
+        <h3>Dashboard</h3>
       </div>
       <div className={styles['menu']}>
         <ul className={styles['menu-list']}> 
-          <li className={styles['menu-item']}>Dashboard</li> 
-          <li className={styles['menu-item']}>Entrega Fornecedor</li>
-          <li className={styles['menu-item']}>Entrega Cliente</li>
-          <li className={styles['menu-item']}>Pedidos Entregues</li>
-          <li className={styles['menu-item']}>faQs</li>
+          <li className={styles['menu-item']}> <img src={dash} alt="Dashboard" /> Dashboard</li> 
+          <li className={styles['menu-item']}> <img src={truck} alt="Pedidos" /> Pedidos</li>
+          <li className={styles['menu-item']}> <img src={pack} alt="Solicitações" /> Solicitações</li>
+          <li className={styles['menu-item']}> <img src={maps} alt="Mapas" /> Mapas</li>
+          <li className={styles['menu-item']}> <img src={faqs} alt="faQs" /> faQs</li>
         </ul>
       </div>
     </div>
