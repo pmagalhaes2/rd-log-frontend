21eda6460f51c4e3c64b8e6a31b7e962774ae897
sophiacontesini <sophiamarques214@gmail.com>
2024-05-14 10:39:14 -0300

    feat: implements requests page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles/Global.css";
-import { Support } from "./pages/Support";
+// import { Support } from "./pages/Support";
+import Requests from "./pages/Requests";
 
 
 function App() {
   return (
     <div className="App">
-      <Support />
+      <Requests />
+    
+  
     </div>
   );
 }
