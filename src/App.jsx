e557fdc11340988f1b4b1faa0f520a1b676dc2f8
sophiacontesini <sophiamarques214@gmail.com>
2024-05-14 11:14:30 -0300

    fix: merge app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import "./styles/Global.css";
-<<<<<<< HEAD
 // import { Support } from "./pages/Support";
 import Requests from "./pages/Requests";
-=======
+
 import { Support } from "./pages/Support";
 import Register from "./Components/Register/Register";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 
->>>>>>> 9c792b431bb13d8f1954889dd300e88ab6558c47
+
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Requests />
     
   
-=======
-      <Header />
+
+       <Header />
       <Register/>
       <Footer />
     </div>
