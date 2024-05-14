6e0d1b5b59cab2c2ccc392939cfdb16a2bf34421
sophiacontesini <sophiamarques214@gmail.com>
2024-05-14 17:32:42 -0300

    feat: change truck image and ajust request size.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import "./styles/Global.css";
-// import { Support } from "./pages/Support";
 import Requests from "./pages/Requests";
-
 import { Support } from "./pages/Support";
 import { Login } from "./pages/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register/Register";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Header/Header";
 
 
 
@@ -17,25 +12,20 @@ import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
 
-      <Requests />
-    
-  
-
-       <Header />
-      <Register/>
     <UserProvider>
       <BrowserRouter>
-        <Header />
+
         <Routes>
           <Route path="/login" Component={Login} />
           <Route path="/register" Component={Register} />
+          <Route path="/requests" Component={Requests}/>
           <Route path="/support" Component={Support} />
         </Routes>
       </BrowserRouter>
-      <Footer />
     </UserProvider>
+    </div>
   );
 }
 
