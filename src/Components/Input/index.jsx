e9ec5d5b5542419aa9a717745394858af8de056e
pmagalhaes2 <paath.magalhaes2@gmail.com>
2024-06-07 15:39:40 -0300

    chore: add mask prop

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Input.module.scss";
 import { Icon } from "@iconify/react/dist/iconify";
 import searchIcon from "@iconify-icons/mdi/magnify";
+import ReactInputMask from "react-input-mask";
 
 export const Input = React.forwardRef(
   (
@@ -16,27 +17,55 @@ export const Input = React.forwardRef(
       freeSize = true,
       defaultValue,
       disabled = false,
-      onClick
+      onClick,
+      mask,
     },
     ref
   ) => {
-    return (
-      <div className={`${styles.container} ${freeSize ? styles.freeSize : ""}`}>
-        {label && <label>{label}</label>}
-        <div className={styles.input_container}>
+    const inputElement = mask ? (
+      <ReactInputMask
+        mask={mask}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {(inputProps) => (
           <input
+            {...inputProps}
             ref={ref}
-            onChange={onChange}
             placeholder={placeholder}
-            value={value}
             type={type}
             required={required}
             className={freeSize ? styles.fullWidth : ""}
             defaultValue={defaultValue}
-            disabled={disabled}
           />
+        )}
+      </ReactInputMask>
+    ) : (
+      <input
+        ref={ref}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        type={type}
+        required={required}
+        className={freeSize ? styles.fullWidth : ""}
+        defaultValue={defaultValue}
+        disabled={disabled}
+      />
+    );
+
+    return (
+      <div className={`${styles.container} ${freeSize ? styles.freeSize : ""}`}>
+        {label && <label>{label}</label>}
+        <div className={styles.input_container}>
+          {inputElement}
           {searchInput && (
-            <Icon icon={searchIcon} className={styles.search_icon} onClick={onClick} />
+            <Icon
+              icon={searchIcon}
+              className={styles.search_icon}
+              onClick={onClick}
+            />
           )}
         </div>
       </div>
