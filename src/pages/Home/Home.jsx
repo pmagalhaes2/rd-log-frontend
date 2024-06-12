5ba1c196f35c066ac3fbd584a41e0216d2932ec0
Cristina-Giardini <crisgiardini30@gmail.com>
2024-06-12 11:41:42 -0300

    refinamento da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,43 @@
 import React from "react";
 import styles from './Home.module.scss';
-import cd1 from '../../assets/images/cd1.jpeg';
-import cd2 from '../../assets/images/cd2.jpeg';
-import cd3 from '../../assets/images/cd3.jpeg';
+import imgcaminho from "../../assets/images/caminhao 1.svg";
+// import cd1 from '../../assets/images/cd1.jpeg';
+// import cd2 from '../../assets/images/cd2.jpeg';
+import cd4 from '../../assets/images/cd4.jpeg';
 
 function Home() {
   return (
-    <div className={styles.container}>
-
-      <div className={styles.box}>
-        <h1> RDlog </h1>
-        <p>A RDLog é uma empresa de logística inovadora,
-           especializada em transporte sustentável com baixa emissão de carbono. 
-           Com um foco em eficiência, a empresa utiliza tecnologias avançadas para otimizar rotas e reduzir o consumo de combustível.
-           Além de sustentável, a RDLog se destaca pela segurança de suas operações, garantindo a integridade das cargas transportadas.
-           O compromisso com a rapidez é outro diferencial, assegurando entregas pontuais e ágeis.
-           A RDLog está redefinindo o setor logístico, alinhando responsabilidade ambiental com excelência operacional.</p>
-        <div className={styles.card}>
-          <div>
-            <img src={cd1} alt="cd1" />
-            <p>Eficiência em logística</p>
-          </div>
-          <div>
-            <img src={cd2} alt="cd2" />
-            <p>Rotas otimizadas</p>
-          </div>
-          <div>
-            <img src={cd3} alt="cd3" />
-            <p>Menos emissão de carbono</p>
-          </div>
+   <div className={styles.container}>
+      <div className={styles.topSection}>
+        <div className={styles.homeBox}>
+          <img
+            className={styles.imagemCaminhao}
+            src={imgcaminho}
+            alt="imagem-box"
+          />
         </div>
+        <div className={styles.teste}>     
+          <div className={styles.rightSection}>
+             <div className={styles.homeSide}>
+               <h2>Juntos por uma entrega mais eficiente e com menos emissão de carbono</h2>
+             </div>
+          </div>
+             <div className={styles.homeSpace}></div>
+     </div>
+     </div>   
+         <div className={styles.homeContent}>
+           <div className={styles.textSection}>
+            <h1>Explore nossas iniciativas:</h1>
+            <h3>Mais produtos, mais entregas e mais saúde a cada rota</h3>
+           </div>
+             <img
+              className={styles.homeImage}
+              src={cd4}
+              alt="imagem cd"
+           />
       </div>
     </div>
+
   );
 }
 
